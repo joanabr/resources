418ae47cc1f3962a87a53a26d7d86f48fb3025f4
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-29 18:34:34 +0200

    Added routes

---
+++ src/Routes/Admin/routes.resources.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @copyright 2018 interactivesolutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Contact InteractiveSolutions:
+ * E-mail: hello@interactivesolutions.lt
+ * http://www.interactivesolutions.lt
+ */
+
+declare(strict_types = 1);
+
+Route::prefix(config('hc.admin_url'))
+    ->namespace('Admin')
+    ->middleware(['web', 'auth'])
+    ->group(function() {
+
+        Route::get('resource', 'HCResourceController@index')
+            ->name('admin.resource.index')
+            ->middleware('acl:honey_comb_resources_resource_admin_list');
+
+
+        Route::prefix('api/resource')->group(function() {
+
+            Route::get('/', 'HCResourceController@getListPaginate')
+                ->name('admin.api.resource')
+                ->middleware('acl:honey_comb_resources_resource_admin_list');
+
+
+            Route::get('list', 'HCResourceController@getList')
+                ->name('admin.api.resource.list')
+                ->middleware('acl:honey_comb_resources_resource_admin_list');
+
+            /*Route::post('/', 'HCResourceController@store')
+                ->name('admin.api.resource.create')
+                ->middleware('acl:honey_comb_resources_resource_admin_create');*/
+
+            Route::delete('/', 'HCResourceController@deleteSoft')
+                ->name('admin.api.resource.delete')
+                ->middleware('acl:honey_comb_resources_resource_admin_delete');
+
+            Route::delete('force', 'HCResourceController@deleteForce')
+                ->name('admin.api.resource.delete.force')
+                ->middleware('acl:honey_comb_resources_resource_admin_delete_force');
+
+            Route::post('restore', 'HCResourceController@restore')
+                ->name('admin.api.resource.restore')
+                ->middleware('acl:honey_comb_resources_resource_admin_restore');
+
+            /*Route::prefix('{id}')->group(function() {
+
+                Route::get('/', 'HCResourceController@getById')
+                    ->name('admin.api.resource.single')
+                    ->middleware('acl:honey_comb_resources_resource_admin_list');
+
+                Route::get('/', 'HCResourceController@update')
+                    ->name('admin.api.resource.update')
+                    ->middleware('acl:honey_comb_resources_resource_admin_update');
+
+                Route::get('/', 'HCResourceController@patch')
+                    ->name('admin.api.resource.patch')
+                    ->middleware('acl:honey_comb_resources_resource_admin_update');
+            });*/
+        });
+
+    });
