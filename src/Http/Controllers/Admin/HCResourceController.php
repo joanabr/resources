265bf16966cc6266dfb0ed57eb89335b14f5e88e
Mantas Paulauskas <mantopastas@gmail.com>
2018-01-30 17:30:20 +0200

    added getListPaginate method

--- src/Http/Controllers/Admin/HCResourceController.php
+++ src/Http/Controllers/Admin/HCResourceController.php
@@ -1,17 +1,47 @@
 <?php
+/**
+ * @copyright 2018 interactivesolutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Contact InteractiveSolutions:
+ * E-mail: info@interactivesolutions.lt
+ * http://www.interactivesolutions.lt
+ */
 
 declare(strict_types = 1);
 
 namespace HoneyComb\Resources\Http\Controllers\Admin;
 
-use HoneyComb\Resources\Services\HCResourceService;
 use HoneyComb\Core\Http\Controllers\HCBaseController;
 use HoneyComb\Core\Http\Controllers\Traits\HCAdminListHeaders;
+use HoneyComb\Resources\Services\HCResourceService;
 use HoneyComb\Starter\Helpers\HCFrontendResponse;
 use Illuminate\Database\Connection;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
+/**
+ * Class HCResourceController
+ * @package HoneyComb\Resources\Http\Controllers\Admin
+ */
 class HCResourceController extends HCBaseController
 {
     use HCAdminListHeaders;
@@ -77,5 +107,17 @@ class HCResourceController extends HCBaseController
         ];
 
         return $columns;
+    }
+
+    /**
+     * Creating data list
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getListPaginate(Request $request): JsonResponse
+    {
+        return response()->json(
+            $this->service->getRepository()->getListPaginate($request)
+        );
     }
 }
