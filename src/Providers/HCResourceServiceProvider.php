584b17bfbf1bd024dc4aa1c016c671051b1a967a
joanabr <38035924+joanabr@users.noreply.github.com>
2018-10-12 11:38:30 +0300

    wip

--- src/Providers/HCResourceServiceProvider.php
+++ src/Providers/HCResourceServiceProvider.php
@@ -24,12 +24,16 @@
  * E-mail: hello@interactivesolutions.lt
  * http://www.interactivesolutions.lt
  */
+
 declare(strict_types = 1);
+
 namespace HoneyComb\Resources\Providers;
+
 use HoneyComb\Resources\Repositories\Admin\HCResourceRepository;
 use HoneyComb\Resources\Services\HCResourceService;
-use HoneyComb\Resources\Console\HCGenerateDefaultPreviewThumbnails;
 use HoneyComb\Starter\Providers\HCBaseServiceProvider;
+use HoneyComb\Resources\Console\HCGenerateDefaultPreviewThumbnails;
+
 /**
  * Class HCResourceServiceProvider
  * @package HoneyComb\Resources\Providers
@@ -40,37 +44,44 @@ class HCResourceServiceProvider extends HCBaseServiceProvider
      * @var string
      */
     protected $homeDirectory = __DIR__;
+
     /**
      * Console commands
      *
      * @var array
      */
     protected $commands = [
-        HCGenerateDefaultPreviewThumbnails::class
+        HCGenerateDefaultPreviewThumbnails::class,
     ];
+
     /**
      * Controller namespace
      *
      * @var string
      */
     protected $namespace = 'HoneyComb\Resources\Http\Controllers';
+
     /**
      * Provider name
      *
      * @var string
      */
     protected $packageName = 'HCResource';
+
     /**
      *
      */
     public function register(): void
     {
         $this->mergeConfigFrom(
-            $this->packagePath('config/resources.php'), 'hc'
+            $this->packagePath('config/resources.php'), 'resources'
         );
+
         $this->registerRepositories();
+
         $this->registerServices();
     }
+
     /**
      *
      */
@@ -78,6 +89,7 @@ class HCResourceServiceProvider extends HCBaseServiceProvider
     {
         $this->app->singleton(HCResourceRepository::class);
     }
+
     /**
      *
      */
